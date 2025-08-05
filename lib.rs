73d6313b05916003e5f0489272e84ffd1a127f6e
Bikash <bikashjha490@gmail.com>
2025-08-05 15:46:07 +0545

    removed line solpg gave

--- lib.rs
+++ lib.rs
@@ -25,9 +25,9 @@ entrypoint!(process_instruction);
 
 // Program entrypoint's implementation
 pub fn process_instruction(
-    program_id: &Pubkey, // Public key of the account the hello world program was loaded into
-    accounts: &[AccountInfo], // The account to say hello to
-    _instruction_data: &[u8], // Ignored, all helloworld instructions are hellos
+    program_id: &Pubkey, 
+    accounts: &[AccountInfo], 
+    _instruction_data: &[u8], 
 ) -> ProgramResult {
 let acc = next_account_info(&mut accounts.iter())?;
 
